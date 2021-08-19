815b65dcb3b513b57fec415543ba64619248ff70
J0e1an <jaycccca@gmail.com>
2021-08-19 14:18:11 -0600

    Create twitter-stream-initiator.sh

---
+++ twitter-stream-initiator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export BEARER_TOKEN=###Your encrypted bearer token here####
+/usr/bin/python3 /path_to_your_streamer.py/streamer.py >> /var/log/twitter-streamer.log 2>&1 &
+jobid=$(echo $!)
+while [ -n "$jobid" ];
+        do
+                if [ -n "$jobid" -a -e /proc/$jobid ];
+                then
+                        echo "twitter stream is up" > /dev/null
+                        sleep 1
+                else
+                        echo "twitter stream is down! $(date)" >> /var/log/twitter-streamer.log
+                        break
+                fi
+        done
