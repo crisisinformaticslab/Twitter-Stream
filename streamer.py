dc2375b29fd4dd235fd4961bbeae804b4992e196
J0e1an <jaycccca@gmail.com>
2021-08-20 13:53:35 -0600

    Update streamer.py

--- streamer.py
+++ streamer.py
@@ -34,13 +34,11 @@ def kinesis(data):
 
     kinesis_client = boto3.client('kinesis')
     try:
-        job_id = json.loads(data)['matching_rules'][0]['id']
-        kinesis_client.put_record(StreamName=kinesis_name,
-                                  Data=data,
-                                  PartitionKey=str(job_id)
-                                  )
+    kinesis_client.put_record(StreamName=kinesis_name,
+                              Data=data,
+                              PartitionKey="Twitter"
+                              )
     except ClientError as e:
-        print(datetime.datetime.now())
         logging.error(e)
         initiator()
 
@@ -49,26 +47,18 @@ def kinesis(data):
 def get_stream(headers):
     # Tone down your additonal fields here, you may not need all these
     additional_fields = "tweet.fields=attachments%2Cauthor_id%2Ccontext_annotations%2Cconversation_id%2Ccreated_at%2Centities%2Cgeo%2Cid%2Cin_reply_to_user_id%2Clang%2Cpublic_metrics%2Cpossibly_sensitive%2Creferenced_tweets%2Creply_settings%2Csource%2Ctext%2Cwithheld&user.fields=created_at%2Cdescription%2Centities%2Cid%2Clocation%2Cname%2Cpinned_tweet_id%2Cprofile_image_url%2Cprotected%2Cpublic_metrics%2Curl%2Cusername%2Cverified%2Cwithheld&expansions=attachments.poll_ids%2Cattachments.media_keys%2Cauthor_id%2Centities.mentions.username%2Cgeo.place_id%2Cin_reply_to_user_id%2Creferenced_tweets.id%2Creferenced_tweets.id.author_id"
-    try:
-        response = requests.get(
-            "https://api.twitter.com/2/tweets/search/stream?" + additional_fields, headers=headers, stream=True,
-        )
-        if response.status_code != 200:
-            raise Exception(
-                "Cannot get stream (HTTP {}): {}".format(
-                    response.status_code, response.text
-                )
+    response = requests.get(
+        "https://api.twitter.com/2/tweets/search/stream?" + additional_fields, headers=headers, stream=True,
+    )
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot get stream (HTTP {}): {}".format(
+                response.status_code, response.text
             )
-        for response_line in response.iter_lines():
-            if response_line:
-                json_response = json.loads(response_line)
-                kinesis(json.dumps(json_response, sort_keys=True))
-            else:
-                print("No response. Waiting for response.")
-    except Exception as e:
-        print(datetime.datetime.now())
-        logging.error(traceback.format_exc())
-        initiator()
+        )
+    for response_line in response.iter_lines():
+        # print(response_line)
+        kinesis(response_line)
 
 
 def initiator():
